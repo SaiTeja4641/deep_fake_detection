f64467c96cab125179443b5f0432ce08a467dfd5
SaiTeja4641 <saisai333asp@gmail.com>
2025-04-04 16:40:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import cv2
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import BinaryCrossentropy
+from sklearn.metrics import accuracy_score
